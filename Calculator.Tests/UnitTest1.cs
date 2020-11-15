1ebf1ab250491e6a1c3fb59f8acb69101a1582be
Алексей Козлов <aaaa.aaaa.aaa.aaaa@mail.ru>
2020-11-15 17:56:46 +0500

    Add Unit test

---
+++ Calculator.Tests/UnitTest1.cs
@@ -0,0 +1,114 @@
+using System;
+using Xunit;
+using Calc;
+
+namespace Calculator.Tests
+{
+    public class CalcTests
+    {
+        [Fact]
+        public void Sum_2Plus5_Returned()
+        {
+            var calc = new ParserCalc();
+            var res = calc.calculate("2 + 5");
+            
+            Assert.Equal("7", res);
+        }
+        [Fact]
+
+        public void Div_7Div3_Returned()
+        {
+            var calc = new ParserCalc();
+            var res = calc.calculate("7 / 3");
+            Assert.Equal(Convert.ToString(7.0 / 3), res);
+        }
+ 
+        [Fact]
+        public void Mul_10MulUnderfined_Returned()
+        {
+            var calc = new ParserCalc();
+            var res = calc.calculate("10*");
+            Assert.Equal("error", res);
+        }
+        [Fact]
+        public void Div_UnderfinedDiv6_Returned()
+        {
+            var calc = new ParserCalc();
+            var res = calc.calculate("/ 6");
+            Assert.Equal("error", res);
+        }
+
+        [Fact]
+        public void Exp_8Plus2Mul10_Returned()
+        {
+            var calc = new ParserCalc();
+            var res = calc.calculate("8+2*10+3");
+            Assert.Equal("31", res);
+        }
+
+        [Fact]
+        public void Exp_10Div3Mul6Plus3_Returned()
+        {
+            var calc = new ParserCalc();
+            var res = calc.calculate("10/3*6+3");
+            Assert.Equal("23", res);
+        }
+        [Fact]
+        public void Exp_1Div2Mul3PlusUnderfined_Returned()
+        {
+            var calc = new ParserCalc();
+            var res = calc.calculate("1/2*3+");
+            Assert.Equal("error", res);
+        }
+        [Fact]
+        public void Exp_5Div2point5_Returned()
+        {
+            var calc = new ParserCalc();
+            var res = calc.calculate("5/2,5");
+            Assert.Equal("2", res);
+        }
+        [Fact]
+        public void Exp_10Plus20Mul2Div4Minus10Mul2point5_Returned()
+        {
+            var calc = new ParserCalc();
+            var res = calc.calculate("10+20*2/4-10*2,5");
+            Assert.Equal("-5", res);
+        }
+        [Fact]
+        public void Exp_4PlusBracket2Plus3BracketMul4DivBracket10Minus5Bracket_Returned8()
+        {
+            var calc = new ParserCalc();
+            var res = calc.calculate("4+(2+3)* 4 / ( 10 - 5)");
+            Assert.Equal("8", res);
+        }
+        [Fact]
+        public void Exp_BrBrBr10Mul5BrBrBrPlusBr2Br_Return52()
+        {
+            var calc = new ParserCalc();
+            var res = calc.calculate("(((10*5))) + (2)");
+            Assert.Equal("52", res);
+        }
+        [Fact]
+        public void BigExpressionTest()
+        {
+            var calc = new ParserCalc();
+            var res = calc.calculate("((4))+(2*3)-(1+2)*3-(10/((7-2)-1))");
+            Assert.Equal("-1,5", res);
+        }
+
+        [Fact]
+        public void Exp_Br10Plus5_ReturnError()
+        {
+            var calc = new ParserCalc();
+            var res = calc.calculate("(10+5");
+            Assert.Equal("error", res);
+        }
+        [Fact]
+        public void Exp_10Plus5Br_ReturnError()
+        {
+            var calc = new ParserCalc();
+            var res = calc.calculate("10+5)");
+            Assert.Equal("error", res);
+        }
+    }
+}
---
+++ WpfApp1/ParserCalc.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Calc 
+{
+    public class ParserCalc
+    {
+        public string calculate(string exp)
+        {
+            exp = RemoveBrackets(exp);
+            int[] arrayIndexOp = FillArrayOfSymbols(exp, new char[] { '-', '+', '*', '/' });
+
+            var arrValue = exp.Split(new char[] { '-', '+', '*', '/' });
+            string result = arrValue[0];
+
+            for (int i = 0; i < arrValue.Length; i++)
+            {
+                int num; double double_num;
+                if (Int32.TryParse(arrValue[i], out num) == false &&
+                    Double.TryParse(arrValue[i], out double_num) == false)
+                    return "error";
+            }
+                
+
+            result = SolveExp(arrayIndexOp, arrValue, exp);
+            return result;
+        }
+
+        public string RemoveBrackets(string exp)
+        {
+            int countStartBracket = exp.Split('(').Length - 1;
+            int countEndBracket = exp.Split(')').Length - 1;
+            if (countStartBracket != countEndBracket) return "error";
+            if ((countStartBracket + countEndBracket) == 0) return exp;
+
+            int[] arrIndexBrackets = FillArrayOfSymbols(exp, new char[] { '(', ')' });
+            
+            int queue = 0;          // queue - очередь скобок. Т.к. по умолчанию открыта одна скобка, то ожидается одна закрывающая (поэтому очередь пока состоит из одной закр. скобки)
+            int startBracket = -1;
+            for (int i = 0; i < arrIndexBrackets.Length ; i++)
+            { 
+                char bracket = exp[arrIndexBrackets[i]];
+                if (queue < 0) return "error";
+                if (bracket == '(' && queue == 0)
+                    startBracket = arrIndexBrackets[i];
+
+
+                if (bracket == ')' && queue == 1)
+                {                                                                           // Добавить бы проверку на пустое содержимое скобок
+                    int endBracket = arrIndexBrackets[i];
+                    string subexp = exp.Substring(startBracket + 1, endBracket - startBracket - 1);
+                    string result = calculate(subexp);
+                    if (result == "error") return "error";
+                    exp = exp.Remove(startBracket, endBracket - startBracket + 1);
+                    exp = exp.Insert(startBracket, result);
+                    arrIndexBrackets = FillArrayOfSymbols(exp, new char[] { '(', ')' });
+                    i = -1;
+                    queue = 0;
+                }
+                else if (bracket == ')')
+                    queue--;
+                else if (bracket == '(')
+                    queue++;
+            }
+
+            if (queue != 0)
+                return "error";
+
+            return exp;
+        }
+
+        public string SolveExp(int[] arrOp, string[] arrValue, string exp)
+        {
+            for (int i = 0; i < arrOp.Length; i++)
+            {
+                string value = "";
+                if (exp[arrOp[i]] == '*')
+                    value = Convert.ToString(Double.Parse(arrValue[i]) * Double.Parse(arrValue[i + 1]));
+                else if (exp[arrOp[i]] == '/')
+                    value = Convert.ToString(Double.Parse(arrValue[i]) / Double.Parse(arrValue[i + 1]));
+                if (value != "")                                                                                    // Присваиваю результат левому операнду в массиве, а правый удаляю
+                {
+                    arrValue[i] = value;
+
+                    Array.Clear(arrValue, i + 1, 1);
+                    for (int index = i + 1; index + 1 < arrValue.Length; index++)
+                        arrValue[index] = arrValue[index + 1];
+                    Array.Resize(ref arrValue, arrValue.Length - 1);
+
+                    Array.Clear(arrOp, i, 1);
+                    for (int index = i; index + 1 < arrOp.Length; index++)
+                        arrOp[index] = arrOp[index + 1];
+                    Array.Resize(ref arrOp, arrOp.Length - 1);
+                    i--;
+                }
+            }
+
+            string result = arrValue[0];
+            for (int i = 0; i < arrOp.Length; i++)
+            {
+                if (exp[arrOp[i]] == '+')
+                    result = Convert.ToString(Double.Parse(result) + Double.Parse(arrValue[i + 1]));
+                if (exp[arrOp[i]] == '-')
+                    result = Convert.ToString(Double.Parse(result) - Double.Parse(arrValue[i + 1]));
+            }
+            return result;
+        }
+        public int[] FillArrayOfSymbols( string exp, char[] symbols) // Заносит в массив индексы всех символов в выражении
+        {
+            int[] arrOfIndexSymbols = new int[100];
+            int size = 0;
+
+            for (int i = 0; i < symbols.Length; i++)
+            {
+                int indexSymbol = -1;
+                char symbol = symbols[i];
+                while (exp.Contains(symbol))
+                {
+                    indexSymbol = exp.IndexOf(symbol);
+                    arrOfIndexSymbols[size++] = indexSymbol;
+                    exp = exp.Remove(indexSymbol, 1);
+                    exp = exp.Insert(indexSymbol, " ");
+                }
+            }
+
+            Array.Resize(ref arrOfIndexSymbols, size);
+            Array.Sort(arrOfIndexSymbols);
+            return arrOfIndexSymbols;
+        }
+    }
+}
